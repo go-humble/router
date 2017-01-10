d36a64c9dbcf87ea8b9b112f0a9a6a40caa027fd
Matt Vinall <Matt.Vinall@imgtec.com>
2017-01-10 11:01:50 +0000

    add continue to IterateLinks, for clarity

--- router.go
+++ router.go
@@ -207,15 +207,18 @@ func (r *Router) InterceptLinks() {
 		href := link.GetAttribute("href")
 		switch {
 		case href == "":
+			continue
 
 		case strings.HasPrefix(href, "http://"), strings.HasPrefix(href, "https://"), strings.HasPrefix(href, "//"):
 			// These are external links and should behave normally.
+			continue
 
 		case strings.HasPrefix(href, "#"):
 			// These are anchor links and should behave normally.
 			// Recall that even when we are using the hash trick, href
 			// attributes should be relative paths without the "#" and
 			// router will handle them appropriately.
+			continue
 
 		case strings.HasPrefix(href, "/"):
 			// These are relative links. The kind that we want to intercept.
